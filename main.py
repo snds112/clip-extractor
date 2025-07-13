1c2bc9b277bb3279ae9851420561a9300df38bd4
snds <se.m.soundous@gmail.com>
2025-07-13 23:03:38 +0100

    added support for folder and file names with spaces

--- main.py
+++ main.py
@@ -23,6 +23,6 @@ if __name__ == "__main__":
     for clip in clips_dict:
         print(clip)
         extension = files[int(clip["episode"])-1].split('.')[-1]
-        os.system("ffmpeg -n -ss "+ clip["start time"] +  " -to "+ clip["end time"] + " -i ../"+files[int(clip["episode"])-1] + " -reset_timestamps 1 -map 0 -c copy -avoid_negative_ts 1 "+ clip["name"]+"."+extension)
+        os.system("ffmpeg -n -ss "+ clip["start time"] +  " -to "+ clip["end time"] + " -i '../"+files[int(clip["episode"])-1] + "' -reset_timestamps 1 -map 0 -c copy -avoid_negative_ts 1 '"+ clip["name"]+"."+extension+"'")
         
         
