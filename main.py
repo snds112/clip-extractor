05eebb1a46ea49b919dca302fd980ce24be1a1a0
snds <se.m.soundous@gmail.com>
2025-07-15 14:20:53 +0100

    added better clip file support (.csv, .xlsx, or .xls)

--- main.py
+++ main.py
@@ -5,18 +5,20 @@ from folder_handler import get_episodes
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Analyze data from a specified file.")
-    parser.add_argument("clips_csv_filepath", type=str, help="Path to the csv file.")
+    parser.add_argument("filepath", type=str, help="Path to the clips sheet.")
     
     args = parser.parse_args()
     
-    try:
-        clips_df= pd.read_csv(args.clips_csv_filepath)
-        print(clips_df)
-        files=get_episodes()
-        print(files)
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        
+    if args.filepath.endswith('.csv'):
+        clips_df = pd.read_csv(args.filepath)
+    elif args.filepath.endswith(('.xlsx', '.xls')):
+        clips_df = pd.read_excel(args.filepath)
+    else:
+        raise ValueError("Unsupported file format. Use .csv, .xlsx, or .xls")
+
+    files=get_episodes()
+    print(files)
+   
     clips_dict= clips_df.to_dict("records")
     os.system("mkdir clips")
     os.chdir("clips")
